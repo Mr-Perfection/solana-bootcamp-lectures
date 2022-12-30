69b635a00a8aa0f5739f2fac1e80b766fde8ece2
Stephen Lee <sungsoolee0127@gmail.com>
2022-12-29 16:33:39 -0800

    add error

--- project_specs/echo-skeleton/program/src/error.rs
+++ project_specs/echo-skeleton/program/src/error.rs
@@ -4,8 +4,28 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Copy, Clone, FromPrimitive, PartialEq)]
 pub enum EchoError {
+    #[error("Account must be writable.")]
+    AccountMustBeWritable,
+    #[error("Account not initialized.")]
+    AccountNotInitialized,
+    #[error("Account contains non-zero data.")]
+    AccountHasNonZeroData,
+    #[error("Missing required signature.")]
+    MissingRequiredSignature,
+    #[error("Invalid program address.")]
+    InvalidProgramAddress,
+    #[error("Invalid account address.")]
+    InvalidAccountAddress,
+    #[error("Invalid instruction input.")]
+    InvalidInstructionInput,
+    #[error("Invalid account data.")]
+    InvalidAccountData,
+    #[error("Default error.")]
+    DefaultError,
     #[error("Instruction not implemented.")]
     NotImplemented,
+    #[error("Insufficient funds.")]
+    InsufficientFunds,
 }
 
 impl From<EchoError> for ProgramError {
