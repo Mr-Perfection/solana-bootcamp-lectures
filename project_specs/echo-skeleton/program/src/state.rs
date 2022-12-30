adc344649ca2b076a8fd4d0ba8761c8652c95d20
Stephen Lee <sungsoolee0127@gmail.com>
2022-12-29 16:33:53 -0800

    add state

--- project_specs/echo-skeleton/program/src/state.rs
+++ project_specs/echo-skeleton/program/src/state.rs
@@ -1,11 +1,18 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use std::mem::size_of;
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub struct AuthorizedBufferHeader {
-    // TODO
+    pub bump_seed: u8,
+    pub buffer_seed: u64,
 }
+
+pub const AUTH_BUFF_HEADER_SIZE: usize = size_of::<u8>() + size_of::<u64>();
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub struct VendingMachineBufferHeader {
-    // TODO
+    pub bump_seed: u8,
+    pub price: u64,
 }
+
+pub const VENDING_MACHINE_BUFF_HEADER_SIZE: usize = size_of::<u8>() + size_of::<u64>();
